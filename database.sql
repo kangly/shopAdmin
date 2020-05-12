acd9dae199b3940fb959a838a8fe2e70e17de738
kangly <371976974@qq.com>
2020-05-12 16:03:39 +0800

    sql

--- database.sql
+++ database.sql
@@ -54,6 +54,47 @@ UNLOCK TABLES;
 
 
 
+# Dump of table shop_product
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `shop_product`;
+
+CREATE TABLE `shop_product` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '商品表id',
+  `title` varchar(100) NOT NULL COMMENT '名称',
+  `desc` text NOT NULL COMMENT '描述',
+  `image` varchar(255) NOT NULL COMMENT '封面图片',
+  `is_sale` tinyint(1) unsigned NOT NULL DEFAULT '1' COMMENT '是否上架',
+  `sold_count` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '销量',
+  `review_count` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '评论数',
+  `price` decimal(10,2) NOT NULL COMMENT '单价',
+  `created_time` timestamp NOT NULL COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_title` (`title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table shop_product_sku
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `shop_product_sku`;
+
+CREATE TABLE `shop_product_sku` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '商品sku表id',
+  `product_id` int(10) unsigned NOT NULL COMMENT '商品表id',
+  `title` varchar(100) NOT NULL COMMENT 'sku名称',
+  `desc` varchar(200) NOT NULL COMMENT 'sku描述',
+  `price` decimal(10,2) NOT NULL COMMENT '单价',
+  `stock` int(10) unsigned NOT NULL COMMENT '库存',
+  `created_time` timestamp NOT NULL COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后更新时间',
+  PRIMARY KEY (`id`),
+  KEY `idx_product_id` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
